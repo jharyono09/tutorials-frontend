be7723567f730c5a8fa157af9282259e85293538
Joni Haryono <jharyono@gmail.com>
2020-07-04 07:38:39 +0700

    some changes

--- server copy.js
+++ server copy.js
@@ -6,10 +6,6 @@ const app = express();
 
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
-app.get(/.*/, function(reg, res) {
-  res.sendfile(__dirname + "/dist/index.html");
-});
-
 const port = process.env.PORT || 8080;
 app.listen(port);
 
